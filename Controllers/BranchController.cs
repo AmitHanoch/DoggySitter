7af0c080e2dacd271f22a5a007e7ad03c4f8c781
Amit Hanoch <amit.dvirhanoch@gmail.com>
2019-06-11 20:29:36 +0300

    Added rout to ger the branches

--- Controllers/BranchController.cs
+++ Controllers/BranchController.cs
@@ -5,6 +5,8 @@ using DoggySitter.Context;
 using DoggySitter.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,11 +14,25 @@ namespace DoggySitter.Controllers
 {
     public class BranchController : Controller
     {
+        private readonly DoggySitterContext _context;
+
+        public BranchController(DoggySitterContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Branch()
         {
             ViewData["Message"] = "dsadad";
 
             return View();
+        }
+
+        public string GetBranches()
+        {
+            List<Branch> branches = _context.Branches.ToList();
+
+            return JsonConvert.SerializeObject(branches, Formatting.Indented);
         }
 
     }
